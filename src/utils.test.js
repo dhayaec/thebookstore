a24b5c3e5b77a7276d890a68ecd8510de45ed4e8
Dhayanandhan <dhayaec@gmail.com>
2018-07-14 20:49:31 +0530

    fix typo

--- src/utils.test.js
+++ src/utils.test.js
@@ -2,7 +2,7 @@ import { add } from './utils';
 
 describe('utils', () => {
   describe('add', () => {
-    it('shold add two numbers', () => {
+    it('should add two numbers', () => {
       expect(add(2, 2)).toEqual(4);
     });
   });
